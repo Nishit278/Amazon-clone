f048b4e0717fc83b8283a8c6bcb496d4235cdb44
Nishit Bansal <nishitbnsl@gmail.com>
2021-06-16 17:54:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/checkout">
+          <Header />
+          <h1>Checkout</h1>
+        </Route>
+        <Route path="/login">
+          <h1>Login</h1>
+        </Route>
+        <Route path="/">
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <nav className="header">
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          />
+        </Link>
+        <div className="header__search">
+          <input type="text" className="header__searchInput" />
+          <SearchIcon className="header__searchIcon" />
+        </div>
+        <div className="header__nav">
+          {/* {1st Link} */}
+          <Link to="/login" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Hello User</span>
+              <span className="header__optionLineTwo">Sign In</span>
+            </div>
+          </Link>
+          {/* {2nd Link} */}
+          <Link to="/login" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Returns</span>
+              <span className="header__optionLineTwo">& Orders</span>
+            </div>
+          </Link>
+          {/* {3rd Link} */}
+          <Link to="/login" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Your</span>
+              <span className="header__optionLineTwo">Prime</span>
+            </div>
+          </Link>
+          {/* {4th Link} */}
+          <Link to="/checkout" className="header__link">
+            <div className="header__optionBasket">
+              <ShoppingCartOutlinedIcon />
+              <span className="header__optionLineTwo header__basketCount">
+                0
+              </span>
+            </div>
+          </Link>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+}
+
+export default Header;
